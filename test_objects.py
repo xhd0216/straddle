73a946d8821093aa5848691d294c20c33032e9b3
xhd0216 <xhd0216@gmail.com>
2017-11-28 13:12:09 -0800

    Update test_objects.py

--- test_objects.py
+++ test_objects.py
@@ -3,9 +3,15 @@ import objects
 class testObjects(objects):
   def __init__(self):
     objects.__init__(self)
-    self.addRequiredField('abc', str)
-
+    self.addRequiredField('str_field', str)
+    self.addRequiredField('int_field', int)
+    self.addRequiredField('float_field', float)
 def test_inherit_objects():
   t = testObjects()
-  assert t.addKey('abc', 1) == False
+  assert t.addKey('str_field', 1)
+  assert t.addKey('int_field', '1.02')
+  assert t.addKey('float_field', 3)
+  assert t.isValid() 
+  assert t.getKey('int_field') == 1
+  assert t.getKey('float_field') == 3
   
