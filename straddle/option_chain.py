34564de97d959aead1492a4e0ca3258e47423886
xhd0216 <xhd0216@gmail.com>
2017-12-04 22:56:22 -0800

    add option chain parser

--- straddle/option_chain.py
+++ straddle/option_chain.py
@@ -1,10 +1,31 @@
 import os
 import json
 import urllib2
+import HTMLParser
+import datetime, time
 from straddle.objects import objects
 from config.url_config import URLDB
 from straddle.earnings import *
 
+option_date_format = '%B %d, %Y'
+
+def date_to_second(s, fm=option_date_format):
+  try:
+    d = datatime.datetime.strptime(s, fm)
+    r = time.mktime(d.timetuple())
+  except:
+    print 'fail to convert time'
+    return None
+  return r
+
+class optionParser(HTMLParser):
+  def __init__(self):
+    HTMLParser.__init__(self)
+  def hasAttr(self, attrs, key):
+    for x in attrs:
+      if key == x[0]:
+        return x[1]
+    return None
 def GetOptionChainPage():
   udb = URLDB()
   earning_url = udb.getItem('earning')
