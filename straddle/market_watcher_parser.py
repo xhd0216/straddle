0030f77c5c2d826ac073e28f5f1c1ccb6d7f1268
xhd0216 <xhd0216@gmail.com>
2017-12-12 16:20:20 -0800

    Update market_watcher_parser.py

--- straddle/market_watcher_parser.py
+++ straddle/market_watcher_parser.py
@@ -4,6 +4,25 @@ import urllib2
 market_watcher_url = 'https://www.marketwatch.com/investing/%s/%s/options'
 
 table_headings=["Symbol", "Last", "Change", "Vol", "Bid", "Ask", "OpenInt", "Strike", "Symbol", "Last", "Change", "Vol", "Bid", "Ask", "OpenInt"]
+
+def getAttr(attrs, key):
+  for x in attrs:
+    if key == x[0]:
+      return x[1]
+  return None
+
+class MWFormParser(HTMLParser):
+  def __init__(self):
+    HTMLParser.__init__(self)
+    self.form_links=[]
+  def handle_starttag(self, tag, attrs):
+    if tag == 'form':
+      a = getAttr(attrs, 'action')
+      if a != None:
+        self.form_links.append(a)
+  def getLinks(self):
+    return self.form_links
+
 class MarketWatcherParser(HTMLParser):
   def __init__(self):
     HTMLParser.__init__(self)
@@ -13,13 +32,15 @@ class MarketWatcherParser(HTMLParser):
     self.row = []
     self.begin_cell = False
     self.expiration_date = False
+  def doXHRtable(self, b=True):
+    self.begin_table=b
   def handle_starttag(self, tag, attrs):
     if tag == 'table':
-      a = self.getAttrs(attrs, 'class')
+      a = getAttr(attrs, 'class')
       if a != None and 'optiontable' in a:
         self.begin_table = True
     elif tag == 'tr' and self.begin_table:
-      a = self.getAttrs(attrs, 'class')
+      a = getAttr(attrs, 'class')
       if a != None and 'heading' in a and a.find('colhead') == -1:
         # meet heading
         # do nothing
@@ -33,12 +54,12 @@ class MarketWatcherParser(HTMLParser):
         self.row = []
         self.stock_price = True
     elif tag == 'td' and (self.stock_price or self.begin_row):
-      a = self.getAttrs(attrs, 'class')
+      a = getAttr(attrs, 'class')
       if 'acenter' not in a:
         self.begin_cell = True
       self.row.append('-')
     elif tag == 'td':
-      a = self.getAttrs(attrs, 'colspan')
+      a = getAttr(attrs, 'colspan')
       if a != None:
         a = int(a)
         if a > 10: # a == len(table_headings)
@@ -68,16 +89,21 @@ class MarketWatcherParser(HTMLParser):
       if self.stock_price:
         if 'Current price' not in data:
           print '=====', data, '====='
-  def getAttrs(self, attrs, key):
-    for x in attrs:
-      if key == x[0]:
-        return x[1]
-    return None
+
 
 def getOptionMW()
   f = urllib2.urlopen(market_watcher_url % ('stock', 'aapl'))
   print f.getcode()
   g = f.read()
   f.close()
-  p = MarketWatcherParser()
+  p = MWFormParser()
   p.feed(g)
+  for u in p.getLinks():
+    f = urllib2.urlopen('https://www.marketwatch.com' + u)
+    g = f.read()
+    print f.getcode()
+    f.close()
+    q = MarketWatcherParser()
+    q.doXHRtable()
+    q.feed(g)
+
