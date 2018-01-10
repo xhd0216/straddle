96ef887903b04a39e3466b671a3813323ce0f566
xhd0216 <xhd0216@gmail.com>
2018-01-10 09:02:17 -0800

    minor change

--- test/test_insider.py
+++ test/test_insider.py
@@ -1,12 +1,15 @@
 import urllib2
+import os
 from lib.parser import *
 
+## columns in the title
 columns = ['symbol', 'issuer', 'insider name', 'shares', 'price', 'value', 'date']
 
 class insiderParser(myParser):
   def __init__(self):
     myParser.__init__(self)
     self.data_row = []
+    self.table = []
     self.has_data = False
     self.last_symbol = ''
   def handle_starttag(self, tag, attrs):
@@ -16,20 +19,24 @@ class insiderParser(myParser):
   def handle_data(self, data):
     self.has_data = True
     if self.isTagOn('table') and self.isTagOn('tr') and self.isTagOn('td'):
-      self.data_row.append(data)
-      if len(self.data_row) == 1:
-        self.last_symbol = data 
+      if len(data) > 0: ## non-empty data
+        self.data_row.append(data)
+        if len(self.data_row) == 1:
+          self.last_symbol = data 
   def handle_endtag(self, tag):
     if tag == 'td' and self.has_data == False:
       self.data_row.append(self.last_symbol)
     if tag == 'tr':
       if self.isTagOn('table'):
         print self.data_row
+        self.table.append(self.data_row)
         self.data_row = []
     self.tagOff(tag)
 
-s = 'http://www.insider-monitor.com/top10_insider_buys_week.html'
-f = urllib2.urlopen(s)
-g = f.read()
-p = insiderParser()
-p.feed(g)
+def test_insider_page():
+  ## s = 'http://www.insider-monitor.com/top10_insider_buys_week.html'
+  f = open(os.path.join(os.path.dirname(__file__), 'insider.html')
+  g = f.read()
+  f.close()
+  p = insiderParser()
+  p.feed(g)
