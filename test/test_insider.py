8dbf580b774476672fd2b3669d96982d102b21bd
xhd0216 <xhd0216@gmail.com>
2018-01-09 22:50:16 -0800

    parse page

--- test/test_insider.py
+++ test/test_insider.py
@@ -1,6 +1,7 @@
 from HTMLParser import *
-from objects import objects
-
+##from objects import objects
+##from urllib2 import *
+import urllib2
 
 class myParser(HTMLParser):
   def __init__(self):
@@ -18,8 +19,8 @@ class myParser(HTMLParser):
     self.tags[tag] += 1
   def tagOff(self, tag):
     if tag not in self.tags:
-      ## encounter an end tag before start tag, error
-      ## but give a warning instead, this is the html issue, don't handle
+      ## encounter an end tag before a start tag, error
+      ## give a warning, this is an html issue, don't handle
       self.tags[tag] = 1
     self.tags[tag] -= 1
   def isTagOn(self, tag):
@@ -31,6 +32,35 @@ class myParser(HTMLParser):
   def handle_endtag(self, tag):
     self.tagOff(tag)
 
+columns = ['symbol', 'issuer', 'insider name', 'shares', 'price', 'value', 'date']
+
 class insiderParser(myParser):
   def __init__(self):
     myParser.__init__(self)
+    self.data_row = []
+    self.has_data = False
+    self.last_symbol = ''
+  def handle_starttag(self, tag, attrs):
+    self.tagOn(tag)
+    if tag == 'td':
+      self.has_data = False
+  def handle_data(self, data):
+    self.has_data = True
+    if self.isTagOn('table') and self.isTagOn('tr') and self.isTagOn('td'):
+      self.data_row.append(data)
+      if len(self.data_row) == 1:
+        self.last_symbol = data 
+  def handle_endtag(self, tag):
+    if tag == 'td' and self.has_data == False:
+      self.data_row.append(self.last_symbol)
+    if tag == 'tr':
+      if self.isTagOn('table'):
+        print self.data_row
+        self.data_row = []
+    self.tagOff(tag)
+
+s = 'http://www.insider-monitor.com/top10_insider_buys_week.html'
+f = urllib2.urlopen(s)
+g = f.read()
+p = insiderParser()
+p.feed(g)
