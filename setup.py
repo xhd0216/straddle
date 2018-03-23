8ec0a1b0e7773ab8b5e9a54f07db5db1c466de54
Zhizhou Li <zli@voleon.com>
2018-03-23 11:57:25 -0700

    change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Straddle',
-    version='0.1',
+    version='0.1.1',
     author_email='xhd0216@gmail.com',
     url='https://github.com/xhd0216/straddle',
     packages=find_packages(),
