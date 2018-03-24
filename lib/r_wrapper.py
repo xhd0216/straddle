82a6f5a6d18f08726387eb2a50af906bd0958910
Joe Lee <xhd0216@gmail.com>
2018-03-24 11:50:06 -0700

    send stdin to R

--- lib/r_wrapper.py
+++ lib/r_wrapper.py
@@ -1,3 +1,4 @@
+import os
 from subprocess import Popen, PIPE
 import tempfile
 
@@ -15,28 +16,34 @@ Psi        -0.12373512
 Elasticity 31.19259480
 """
 
-lines = output[0].split('\n')
-res = dict()
-for line in lines:
-  lin = line.split()
-  if len(lin) > 1:
-    res[lin[0].strip()] = float(lin[1])
+
+def par(output):
+	lines = output.split('\n')
+	res = dict()
+	for line in lines:
+		lin = line.split()
+		if len(lin) > 1:
+			res[lin[0].strip()] = float(lin[1])
+	return res
+
 
 def greeks(arg_dicts):
 	""" wrapper to call greeks in R
 			input in an array of dicts
 	"""
 	new_file, filename = tempfile.mkstemp()
-
+	print filename
 	os.write(new_file, "this is some content")
 	os.close(new_file)
-	p = Popen(["Rscript", "greeks.R"], stdout=PIPE)
+	p = Popen(["Rscript", "greeks.R"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
 
-	for arg in arg_dicts:
-		pass
-	output = p.communicate()
+	output = p.communicate(input=filename + '\n' + 'abc' + '\n')
+	# output[0] = stdout, output[1] = stderr
 	rc = p.returncode
-
 	if rc != 0:
 	  exit(2)
+	print par(output[0])
+	print output[1]
 
+
+greeks(None)
