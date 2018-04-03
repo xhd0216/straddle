de0bd293c6df32682e8a9b8dc7de2e5c2e298dde
Joe Lee <xhd0216@gmail.com>
2018-04-02 23:51:27 -0700

    working on db insert

--- db/db_connect.py
+++ db/db_connect.py
@@ -3,12 +3,24 @@ import logging
 import sqlalchemy
 
 from mysql_connect import get_mysql_connect
+from straddle.market_watcher_parser import getOptionMW
+from straddle.strategy import Strike
 
 def get_engine(cnf):
   return sqlalchemy.create_engine(get_mysql_connect(cnf))
 
-def insert_options():
+COLUMNS = ['underlying', 'price', 'strike', 'expiration', 'call', 'query_time', 'ask', 'bid', 'last', 'open_int']
+
+def insert_options(eng, s):
+  assert isinstance(s, Strike)
   rows = sqlalchemy.Table('options_test')
+
   i = rows.insert()
-  i.execute({'underlying':'aapl', 'strike':160, 'expiration':'2018-04-20', 'is_call':True, 'query_time':datetime.datetime.now()})
+  i.execute(s.data())
+  #i.execute({'underlying':'aapl', 'price':152.31, 'strike':160, 'expiration':'2018-04-20', 'is_call':True, 'query_time':datetime.datetime.now()})
+
+
+if __name__ == '__main__':
+  row = getOptionMW()[0]
+  insert_options(row)
 
