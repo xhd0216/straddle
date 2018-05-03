15be5145bfdb7366301916d6ae23559ec4c89a7e
xhd0216 <xhd0216@gmail.com>
2018-05-03 11:19:04 -0700

    Create restore-db.sh

---
+++ deployment/restore-db.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+read -r thost tport tuser tpass tdb <<< $(awk -F"=" '{print $2}' test-options.cnf)
+
+# Restore
+cat backup.sql | docker exec -i test-mysql /usr/bin/mysql -u root --password=root options
