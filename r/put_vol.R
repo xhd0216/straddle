e46a64dc33336fd11a8077b8cb48aed0ccb9984d
Joe Lee <xhd0216@gmail.com>
2018-05-05 00:42:44 -0700

    change put_vol

--- r/put_vol.R
+++ r/put_vol.R
@@ -4,12 +4,19 @@ f <- file("stdin")
 open(f)
 while(length(line <- readLines(f, n=1)) > 0) {
   arr <- strsplit(line, "\\s+")
-  tryCatch(print(bsputimpvol(s=as.double(arr[[1]][1]),
-                              k=as.double(arr[[1]][2]),
-                              r=as.double(arr[[1]][3]),
-                              tt=as.double(arr[[1]][4]),
-                              d=0,
-                              price=as.double(arr[[1]][5]))),
-           error=function(e) NA)
+  s <- as.double(arr[[1]][1])
+  k <- as.double(arr[[1]][2])
+  r <- as.double(arr[[1]][3])
+  tt <- as.double(arr[[1]][4])
+  d <- 0
+  price <- as.double(arr[[1]][5])
+  tryCatch({
+            iv <- bsputimpvol(s, k, r, tt, d, price)
+            gr <- greeks(bsput(s, k, iv, r, tt, d))
+            print(c(iv, gr[2], gr[3], gr[4], gr[5], gr[6]))
+            #     impvol, delta, gamma, vega, rho, theta
+         },
+         error=function(e){ NA },
+         warning=function(w) { NA })
   #write(' ', stdout())
 }
