8e3a4b2b2cb06c0c1332a82bb0005360afff728f
xhd0216 <xhd0216@gmail.com>
2018-05-14 18:31:05 -0700

    create test table

--- test/test_db_connection.py
+++ test/test_db_connection.py
@@ -1,29 +1,7 @@
 import sqlalchemy
 
-from sqlalchemy import Table, Column, Integer, Float, String, Date, DateTime, MetaData
-
-
-def create_test_options_table(engine, test_table_name='test_options'):
-  metadata = MetaData()
-  share_table = Table(test_table_name, metadata,
-                      Column('recordid', Integer, primary_key=True),
-                      Column('vfundid', Integer, nullable=False),
-                      Column('class', String(length=15), nullable=False),
-                      Column('fraction', Float, nullable=False),
-                      Column('start_date', Date, nullable=False),
-                      Column('end_date', Date, nullable=False),
-                      Column('created_utc', DateTime, nullable=False),
-                      Column('updated_utc', DateTime, nullable=False))
-  metadata.create_all(engine)
+from straddle.db import db_connect
 
 def test_connect():
   engine = sqlalchemy.create_engine('sqlite://')
-
-  res = engine.execute('show tables;').fetchall()
-  assert res == []
-
-  res = engine.execute('create table if not exists test_table (name varchar(10));')
-
-  res = engine.execute('show tables;').fetchall()
-
-  assert res[0][0] == 'test_table'
+  db_connect.create_test_option_table(engine)
